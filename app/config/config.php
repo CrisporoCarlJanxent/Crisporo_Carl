95dd1c998940b4b5276f160ee88b5409ffae32e8
CrisporoCarlJanxent <cjcrisporo18@gmail.com>
2025-09-24 21:54:36 +0800

    file_up4

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= '';
+$config['base_url'] 				= 'https://crisporo-carl.onrender.com/';
                                     /*'http://localhost/CRUD%20BIT/Crisporo_Carl/'*/ 
                                  
                                     /* 'https://crisporo-carl.onrender.com/'*/
